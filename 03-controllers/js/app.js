b1bde30f53326cf2f0bdee815894ae5291976278
Crysfel Villa <crysfel@bleext.com>
2014-09-11 16:48:50 -0400

    Adding controllers

---
+++ 03-controllers/js/app.js
@@ -0,0 +1,13 @@
+(function(){
+    "use strict";
+
+    angular.module('Bookmarks',[
+        //dependencies here
+    ])
+
+    .controller('MainController',function($scope){
+        $scope.name = 'Carl'
+        $scope.categories = ['HTML5','JavaScript','CSS','Games'];
+    });
+
+})();
